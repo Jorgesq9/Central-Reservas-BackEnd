93c43d22eae8e664728978dbc0e435ab632f2626
Jorge Esquiva <jorgeesquivait@gmail.com>
2025-01-16 02:51:36 +0100

    schema

--- src/models/user.js
+++ src/models/user.js
@@ -4,7 +4,6 @@ const bcrypt = require("bcryptjs");
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  name: { type: String, required: true, trim: true },
   role: { type: String, enum: ["admin", "worker"], default: "worker" },
 });
 
